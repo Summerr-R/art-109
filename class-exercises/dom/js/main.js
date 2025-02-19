50d9252bae3fb5fc55e981c9381dc3fa4b03431c
Summerr-R <rissosummer@gmail.com>
2025-02-19 14:19:03 -0800

    Tut5

---
+++ class-exercises/dom/js/main.js
@@ -0,0 +1,14 @@
+
+
+document.querySelector("#image1").addEventListener("click", function(){
+    document.querySelector("#image1").style.visibility = "hidden";
+})
+document.querySelector("#image2").addEventListener("click", function(){
+    document.querySelector("#image2").style.visibility = "hidden";
+})
+document.querySelector("#image3").addEventListener("click", function(){
+    document.querySelector("#image3").style.visibility = "hidden";
+})
+document.querySelector("#image4").addEventListener("click", function(){
+    document.querySelector("#image4").style.visibility = "hidden";
+})
